a41898326d3804a37732f124ed095cc9f8cffea3
Zack M. Davis <code@zackmdavis.net>
2023-09-29 12:20:15 -0700

    PyTorch polynomial fit
    I'm not actually observing the double-descent phenomenon, but this is
    a foundation for futher experiments.
    
    Thanks to GPT-4 as tutor and code assistant.

--- double_descent.py
+++ double_descent.py
@@ -1,19 +1,28 @@
 import random
-from math import log2
-from collections import deque
+from math import log
 
-class Polynomial:
-    def __init__(self, coefficients):
-        self.coefficients = coefficients
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
-    def __call__(self, x):
-        return sum(c * x**i for i, c in enumerate(self.coefficients))
+class Polynomial(nn.Module):
+    def __init__(self, degree, coefficients=None):
+        super().__init__()
+        if coefficients is not None:
+            self.coefficients = nn.Parameter(torch.tensor(coefficients), requires_grad=False)
+        else:
+            self.coefficients = nn.Parameter(torch.randn(degree + 1, requires_grad=True))
+
+    def forward(self, x):
+        y = sum(c * x ** i for i, c in enumerate(self.coefficients))
+        return y
 
     def __repr__(self):
-        constant = str(self.coefficients[0])
+        coefficients = [c.item() for c in self.coefficients]
+        constant = str(coefficients[0])
         other_terms = (
             (str(c) if c != 1 else "") + "x" + (p if p != "¹" else "")
-            for c, p in zip(self.coefficients[1:], "¹²³⁴⁵⁶⁷⁸⁹") if c != 0
+            for c, p in zip(coefficients[1:], "¹²³⁴⁵⁶⁷⁸⁹") if c != 0
         )
         return (
             (constant + " + " if constant != "0" else "") +
@@ -21,86 +30,59 @@ class Polynomial:
         )
 
 
-def random_polynomial(degree, coefficient_range=(-9,9)):
-    return Polynomial([random.randint(*coefficient_range) for _ in range(degree+1)])
-
-
 def sample_from_polynomial(p, n, domain=(0,10)):
     samples = []
     for _ in range(n):
         x = random.uniform(*domain)
         y = p(x)
-        samples.append((x, y))
+        samples.append((x, y.item()))
     return samples
 
 
-def gradient(p, x, y):
-    return [2 * x**i * (p(x) - y) for i in range(len(p.coefficients))]
+def polynomial_fit(training_set, degree, epochs=20_000, log=False):
+    criterion = nn.MSELoss()
 
+    model = Polynomial(degree)
 
-def log_loss(p, dataset):
-    # print(p)
-    total = 0
-    for x, y in dataset:
-        # print(p, p(x), y, total)
-        total += log2((p(x) - y)**2)
-    return total
+    optimizer = optim.Adam(model.parameters(), lr=0.02)
 
-
-# XXX: broken
-def gradient_descent_polynomial_fit(training_set, degree, max_steps=250000):
-    step_counter = 0
-    p = Polynomial([random.random()] * (degree+1))
-    recent_log_losses = deque([float('inf')])
-    continue_training = True
-
-    while continue_training:
-        # full batch
-        g = [0 for _ in range(degree+1)]
+    for epoch in range(epochs):
+        if log:
+            if epoch % 1000 == 0:
+                print(epoch, model.coefficients)
         for x, y in training_set:
-            contribution = gradient(p, x, y)
-            for i, c in enumerate(contribution):
-                g[i] += c
+            x_tensor = torch.tensor([x], dtype=torch.float32)
+            y_tensor = torch.tensor([y], dtype=torch.float32)
 
-        step_size = 0.01
-        new_coefficients = [c - step_size*d for c, d in zip(p.coefficients, g)]
-        while log_loss(Polynomial(new_coefficients), training_set) >= recent_log_losses[-1] or any(c > 9 for c in new_coefficients):
-            step_size /= 2
-            new_coefficients = [c - step_size*d for c, d in zip(p.coefficients, g)]
-            # XXX: broken
-            if step_size < 1e-30:
-                import pudb; pudb.set_trace()
+            y_pred = model(x_tensor)
 
-        print("gradient, new coeffs", g, new_coefficients)
-        p = Polynomial(new_coefficients)
+            loss = criterion(y_pred, y_tensor)
 
-        new_log_loss = log_loss(p, training_set)
-        recent_log_losses.append(new_log_loss)
-        step_counter += 1
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        if len(recent_log_losses) > 5:
-            recent_log_losses.popleft()
+    return model
 
-            recent_reductions = [
-                recent_log_losses[i] - recent_log_losses[i+1]
-                for i in range(len(recent_log_losses)-1)
-            ]
 
-            if step_counter % 25000 == 0:
-                print("{} steps; reductions {}".format(step_counter, recent_reductions))
+def evaluate_model(model, test_set, criterion):
+    total_loss = 0.0
+    with torch.no_grad():
+        for x, y in test_set:
+            x_tensor = torch.tensor([x], dtype=torch.float32)
+            y_tensor = torch.tensor([y], dtype=torch.float32)
 
-            if step_counter >= max_steps:
-                continue_training = False
-                print("total steps {}".format(step_counter))
-
-    return p
+            y_pred = model(x_tensor)
+            loss = criterion(y_pred, y_tensor)
+            total_loss += loss.item()
+    return log(total_loss)
 
 
 # The obvious experiment to do to reproduce the double-descent phenomenon—
 # 1. Pick a complicated "true" theory.
 # 2. Grab test and training datasets, where the training data isn't enough to
 #    pin down the polynomial.
-# 3. Use `gradient_descent_polynomial_fit` with various degrees, and
+# 3. Use `polynomial_fit` with various degrees, and
 #    measure the loss for each.
 # 4. Prediction: the worst results will happen at the interpolation threshold
 #    equal to the number of training points; higher degrees will do better,
@@ -108,15 +90,14 @@ def gradient_descent_polynomial_fit(training_set, degree, max_steps=250000):
 #    polynomial!!
 
 def double_descent_experiment():
-    ...
-
+    truth = Polynomial(10, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+    training_set = sample_from_polynomial(truth, 5)
+    test_set = sample_from_polynomial(truth, 50)
+    for degree in range(1, 10):
+        model = polynomial_fit(training_set, degree)
+        print(model)
+        loss = evaluate_model(model, test_set, nn.MSELoss())
+        print(degree, loss)
 
 if __name__ == "__main__":
-    truth = Polynomial([6, 5, 4, 3, 2, 1])
-    training_set = sample_from_polynomial(truth, 200)
-    gradient_descent_polynomial_fit(training_set, 5, max_steps=1000000)
-    # truth = Polynomial([-7, -2, 3, 5])
-    # print(truth)
-    # training_set = sample_from_polynomial(truth, 10)
-    # hypothesis = gradient_descent_polynomial_fit(training_set, 3)
-    # print(hypothesis)
+    double_descent_experiment()
